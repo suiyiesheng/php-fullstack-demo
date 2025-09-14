06f140f04771a0e237d814b122d48cb50c05065e
suiyiesheng <278581341@qq.com>
2025-09-14 13:23:25 +0800

    Day1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+//import { ref } from 'vue';
+// const count = ref(0)
+//const msg = "Hello Vue3 with Template!"
+</script>
 
 <template>
-  <h1>Hello World - Vue3</h1>
+  <!-- <h1>{{ msg }}</h1>
+  <p>当前时间：{{ new Date().toLocaleString() }}</p>
+  <button @click="count++">点击了{{ count }}次</button>
+  <p v-if="count > 3">点击次数大于3！</p>
+  <ul>
+    <li v-for="n in count" :key="n">第{{ n }}次点击</li>
+  </ul> -->
+  <HelloWorld name="小明" />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps({
+    name: String
+})
+</script>
+
+<template>
+    <h2>你好,{{ name }}!</h2>
+</template>
